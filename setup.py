f8cb4e7675e9128b67cb0becff386090e920fce5
halprin <halprin@users.noreply.github.com>
2018-10-11 19:11:37 -0600

    Additional distribution details

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='fcli',
     version='1.0.0',
+    author='halprin',
+    author_email='me@halprin.io',
+    description='Helps spread the AwesomeSauce of the Foundational Components team a bit further',
+    long_description='A CLI for the QPP Foundational Components team to use to help them do their day to day job.',
+    url='https://github.com/halprin/fcli',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
     packages=find_packages(),
     install_requires=[
         'click',
