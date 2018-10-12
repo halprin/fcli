d3d17e15b3c0165639c53597618f808504115757
Alexander Stein <astein@flexion.us>
2018-10-12 01:04:21 -0400

    Address PR #14 comments and remove ctor init function.

--- fc/auth/envvarauth.py
+++ fc/auth/envvarauth.py
@@ -3,14 +3,8 @@ import os
 
 
 class EnvVarAuth(Auth):
-    def __init__(self, username=None):
-        self._username = username
-
     def username(self):
-        if self._username is None:
-            return os.environ['FCLI_USER']
-        else:
-            return self._username
-
+        return os.environ['FCLI_USER']
+        
     def password(self):
         return os.environ['FCLI_PASS']
