d04a9383c8641e2dfefb05501af877e86805adc4
halprin <halprin@users.noreply.github.com>
2018-10-16 20:39:14 -0600

    Update setup.py version to 1.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as read_me:
 
 setup(
     name='fcli',
-    version='1.0.0',
+    version='1.1.0',
     author='halprin',
     author_email='me@halprin.io',
     description='Helps spread the AwesomeSauce of the Foundational Components team a bit further',
