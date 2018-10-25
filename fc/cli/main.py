502ef2bb391f7fef470e3b92701ebc9908133ffc
Alexander Stein <astein@flexion.us>
2018-10-25 07:21:07 -0400

    Fix up version info to use --version, not version subcommand.

--- fc/cli/main.py
+++ fc/cli/main.py
@@ -4,9 +4,14 @@ from requests.exceptions import HTTPError
 from ..auth.combo import ComboAuth
 from fc import __version__
 
-@click.group()
-def cli():
-    pass
+@click.group(invoke_without_command=True)
+@click.option('--version', is_flag=True, default=False)
+@click.pass_context
+def cli(ctx, version):
+    if version:
+        click.echo(__version__)
+    else:
+        click.echo(ctx.get_help())
 
 
 @cli.command()
@@ -29,7 +34,3 @@ def task(title, description, parent_story, username, in_progress, no_assign):
             click.echo('Triage task put into In Progress')
     except HTTPError as exception:
         click.echo('{} task creation failed with {}'.format(new_task.type_str(), exception))
-
-@cli.command()
-def version():
-    print(__version__)
